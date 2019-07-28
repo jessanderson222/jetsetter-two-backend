b46b846dfe9d6b3a240493f20192bde8e229091a
jessanderson222 <jessanderson222@gmail.com>
2019-07-28 12:50:33 -0400

    Seed data for countries

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,23 @@
-require 'rest-client'
+require 'net/http'
 require 'json'
 require 'pry'
 
 Country.destroy_all
 
-binding.pry
-
 countryapi_url = 'https://restcountries.eu/rest/v2/all'
-response = RestClient.get(countryapi_url)
-data = JSON.parse(response)
-countrydata = data["Response"]
+uri = URI(countryapi_url)
 
-countrydata.each do |indcountry|
+response = Net::HTTP.get(uri)
+data = JSON.parse(response)
+
+data.each do |indcountry|
    country = Country.new
    country.name = indcountry["name"]
    country.region = indcountry["region"]
    country.subregion = indcountry["subregion"]
-   # country.currency = indcountry["CurrencyName"]
+   indcountry["currencies"].each do |name|
+      country.currency = name["name"]
+   end 
    country.save
 end 
 
